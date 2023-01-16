66c791ac535b09fe5b17c844dea97c75cea87aa7
elmi-elmi <shahrokhelmi@gmail.com>
2023-01-16 16:53:56 +0330

    initializing

---
+++ src/components/MainNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="w-dull text-sm">
+    <div class="fixed w-full left-0 top-0 h-16 bg-white">
+      <div
+        class="flex flex-nowrap h-full mx-auto px-8 border-b border-solid border-brand-gray-1"
+      >
+        <a href="/" class="text-xl h-full flex items-center font-sa"
+          >Bobo Careers</a
+        >
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defualtTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defualtTheme.fontFamily.sans],
+      },
+      colors: {
+        "brand-gray-1": "dadce0",
+        "brand-blue-1": "1967d2",
+        "brand-green-1": "137333",
+      },
+    },
+  },
+  plugins: [],
+};
