6e980368bb6c0d3e44e9c5ca1c6c953ca8437708
elmi-elmi <shahrokhelmi@gmail.com>
2023-01-16 19:14:21 +0330

    unit test config

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,9 +4,10 @@
       <div
         class="flex flex-nowrap h-full mx-auto px-8 border-b border-solid border-brand-gray-1"
       >
-        <a href="/" class="text-xl h-full flex items-center font-sa"
-          >Bobo Careers</a
-        >
+        <a href="/" class="text-xl h-full flex items-center">{{ company }}</a>
+        <h2 class="flex h-full items-center ml-8">
+          Developed by {{ author.firstname }} {{ author.lastname }}
+        </h2>
       </div>
     </div>
   </header>
@@ -15,5 +16,14 @@
 <script>
 export default {
   name: "MainNav",
+  data() {
+    return {
+      company: "Bobo Careers",
+      author: {
+        firstname: "Alireza",
+        lastname: "elmi",
+      },
+    };
+  },
 };
 </script>
