bb46a664e37ef37faa53e758c4e003643f107369
elmi-elmi <shahrokhelmi@gmail.com>
2023-01-19 18:57:58 +0330

    pagination

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -2,12 +2,31 @@
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
       <JobListing
-        v-for="job in jobs"
+        v-for="job in displayedJobs"
         :key="job.id"
         data-test="job-listing"
         :job="job"
       />
     </ol>
+    <div class="mt-8 mx-auto">
+      <div class="flex flex-row flex-nowrap">
+        <p class="text-sm flex-grow">Page {{ currentPage }}</p>
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            >Previous</router-link
+          >
+          <router-link
+            v-if="nextPage"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            >Next</router-link
+          >
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -22,6 +41,30 @@ export default {
     return {
       jobs: [],
     };
+  },
+  computed: {
+    currentPage() {
+      const pageString = this.$route.query.page || "1";
+      return Number.parseInt(pageString);
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      console.log(nextPage);
+      const maxPage = this.jobs.length / 10;
+      console.log(nextPage, maxPage);
+      return nextPage <= maxPage ? nextPage : undefined;
+    },
+    displayedJobs() {
+      const pageNumber = this.currentPage;
+      const firstJobIndex = (pageNumber - 1) * 10;
+      const lastJobIndex = pageNumber * 10;
+      return this.jobs.slice(firstJobIndex, lastJobIndex);
+    },
   },
   async mounted() {
     const url = "http://localhost:3000/jobs";
--- tests/unit/JobResults/Joblistings.test.js
+++ tests/unit/JobResults/Joblistings.test.js
@@ -5,16 +5,26 @@ jest.mock("axios");
 import JobListings from "@/components/JobResults/JobListings";
 
 describe("JobListings", () => {
+  const createRoutes = (query) => ({
+    query: { page: "1", ...query },
+  });
+  const createConfig = ($route) => ({
+    global: {
+      mocks: { $route },
+    },
+  });
   it("fetch jobs", () => {
     axios.get.mockResolvedValue({ data: [] });
-    shallowMount(JobListings);
+    const route = createRoutes({ page: "1" });
+    shallowMount(JobListings, createConfig(route));
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
-  it("create a job listing for each received job", async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-    const wrapper = shallowMount(JobListings);
+  it("create a job listing for for a maximum 10 jobs", async () => {
+    axios.get.mockResolvedValue({ data: Array(35).fill({}) });
+    const route = createRoutes({ page: "2" });
+    const wrapper = shallowMount(JobListings, createConfig(route));
     await flushPromises();
     const jobListings = wrapper.findAll("[data-test='job-listing']");
-    expect(jobListings).toHaveLength(15);
+    expect(jobListings).toHaveLength(10);
   });
 });
