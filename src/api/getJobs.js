851fc6f75384bd2cdb8b79ef7e766584587034ea
elmi-elmi <shahrokhelmi@gmail.com>
2023-01-21 19:31:05 +0330

    return to frontend

--- src/api/getJobs.js
+++ src/api/getJobs.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const getJobs = async () => {
   const url = process.env.VUE_APP_URL || "http://localhost:3000";
-  const response = await axios.get(`${url}/job/results`);
-  console.log("==============", response.data);
+  const response = await axios.get(`${url}/jobs`);
+  // const response = await axios.get(`${url}//job/results`); for backend
   return response.data;
 };
 
